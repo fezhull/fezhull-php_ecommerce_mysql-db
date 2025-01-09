f78157e14f2b6f54e46b79977c79e7d57ee9c5ce
fezhull <felizhielerfezull@gmail.com>
2025-01-09 19:05:24 +0300

    About page

---
+++ about.php
@@ -0,0 +1,189 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.png" alt="">
+      </div>
+
+      <div class="content">
+         <h3>About Henjos:</h3>
+         <p>Welcome to Henjos.Shop, your one-stop online destination for premium product category, e.g., fashion, electronics, home goods. We’re passionate about offering top-quality products at affordable prices, all while delivering a seamless shopping experience that’s both easy and enjoyable.</p>
+
+         <!-- <p>I would like to thank <a href="https://www.facebook.com/henjos.com" target="_blank">henjos.shop</a> for better shooping experice. </p> -->
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> <a href="https://www.facebook.com/henjos.shop" target="_blank">Henjos .shop</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>It is the first online services in kenya. Having it on my bookmark makes it great for me to get things faster.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/henjos.shop" target="_blank">Henjos. shop</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Henjos is great if you choose good sellers . A variety of required item available . Customers can return and refund the amount.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/henjos.shop" target="_blank">henjos. shop</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/henjos. shop" target="_blank">Henjos .shop </a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/henjos.shop" target="_blank">henjos .shop</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.jpg" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products. Everything else has been good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/henjos.shop"  target="_blank">Henjos. shop</a></h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
